bb1dc6b29e1abcc7bf91efa65242e61ac41de50e
Fernando Luiz <feluiz95@gmail.com>
2021-11-21 17:06:53 -0300

    junk

--- development/Hardware/src/env.exemplo.h
+++ development/Hardware/src/env.exemplo.h
@@ -11,5 +11,5 @@ const char* pass = "";  // Password  WiFi network
 const char* token = "xxxxxxx:XXXXXXXXXXX_XXXXXXXXXXX";
 // Check the userid with the help of bot @JsonDumpBot or @getidsbot (work also with groups)
 // https://t.me/JsonDumpBot  or  https://t.me/getidsbot
-int64_t userid = 1565176038;  
+int64_t userid = 11111111;  
 #endif //ENV
